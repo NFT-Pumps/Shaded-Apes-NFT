04f39e90afb347ddfc40c60163fa411d2a573316
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-11-06 22:39:42 -0600

    Initial

---
+++ scripts/deployArgs.js
@@ -0,0 +1,18 @@
+module.exports = [
+ 'Rockin Kittens NFT',
+ 'RKNFT',
+ 
+    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
+    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
+    'https://public-ipfs.s3.us-east-2.amazonaws.com/RockinKittenNFT/json/',
+    'https://public-ipfs.s3.us-east-2.amazonaws.com/RockinKittenNFT/assets/rs.json',
+
+    [
+        ethers.utils.getAddress('0xdb0B9afA62332E08d049bD5975027fF7Ffafe31a'),
+        ethers.utils.getAddress('0xCd43AdcB61949ab14D3f4574BFbDA53d46389715')
+    ],
+    [
+        50,
+        50
+    ]
+]
